2e95f5fa0ca97f5d6cc058c10c282ad83d14bc06
MChapeland <matthieuchapeland@gmail.com>
2023-09-14 16:16:36 +0200

    yertyerty

---
+++ src/search.js
@@ -0,0 +1,39 @@
+function searchMarkers() {
+    
+    document.getElementById('Search').querySelectorAll(".search-result").forEach((node) => {
+        document.getElementById('Search').removeChild(node);
+    });
+
+
+    markersData.forEach((obj) => {
+
+        if (obj.name.toUpperCase().indexOf(document.getElementById('searchingInput').value.toUpperCase()) > -1 ) {
+            
+            let newSearchResult = document.createElement("div");
+            newSearchResult.classList.add('search-result');
+            newSearchResult.id = `SR_${obj.name}`;
+            
+                //In the search result create an icon
+                let newIcon = document.createElement("img");
+                newIcon.src = `src/images/markers/white/${obj.icon.toLowerCase()}.svg`;
+                newSearchResult.appendChild(newIcon);
+
+                //In the search result create a label
+                let newLabel = document.createElement("label");
+                newLabel.innerText = `${obj.name}`;
+                newSearchResult.appendChild(newLabel);
+                
+                //In the search result create a label
+                let newP = document.createElement("p");
+                newP.innerText = `${obj.description}`;
+                newSearchResult.appendChild(newP);
+
+            //Append the whole thing to the Search tab
+            document.getElementById("Search").appendChild(newLayerGroup);
+
+        }
+
+    });
+
+
+}
